85093b6e396c8acc3c021053afabf7d0b52ce4cf
Ana Xavier <ana.xavier@m2msolutions.com.br>
2020-02-13 17:33:04 -0300

    extilizando pagina navegar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 //import '.styles/global';
+import  {BrowserRouter} from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
 //import Meu from './components/Meu';
 //import { Container } from  './components/Sidebar/styles';
@@ -10,6 +12,8 @@ import Player from './components/Player';
 import Header from './components/Header';
 
 import {Wrapper, Container, Content } from './styles/components';
+
+import Routes from './routes';
 
 
 
@@ -29,19 +33,20 @@ export default function App() {
   return (
    
     <>
+    <BrowserRouter>
       <Wrapper>
         <Container >
           <Sidebar />
           <Content>
             <Header />
-
+              <Router />
           </Content>
         </ Container>
         <Player />
-      </Wrapper>
-      
+      </Wrapper>   
+   
       <GlobalStyle />
-      
+      </BrowserRouter> 
     </>
   )
 };
